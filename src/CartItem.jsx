e40e70b4f91fd37a60261fe48110d6a67aa2aa5c
EJ Fernandez <edwardjamesf@gmail.com>
2024-07-25 15:31:44 -0700

    More updates

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,38 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalCost = 0;
+    cart.forEach((item) => {
+      let itemCost = Number(item.cost.replace(/[^0-9.-]+/g,""));
+      totalCost += itemCost * item.quantity;
+    })
+    return totalCost;
   };
 
   const handleContinueShopping = (e) => {
-   
+    dispatch(onContinueShopping(e));
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity(item.quantity + 1))
   };
 
   const handleDecrement = (item) => {
-   
+    dispatch(updateQuantity(item.quantity - 1))
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let itemCost = Number(item.cost.replace(/[^0-9.-]+/g,""));
+    return itemCost * item.quantity;
   };
 
   return (
